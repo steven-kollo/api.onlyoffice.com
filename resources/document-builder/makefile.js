ddded3299ae57f7df92b18425a155c8d4a05241c
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-01-20 03:42:43 +0400

    add eslint for examples

--- resources/document-builder/makefile.js
+++ resources/document-builder/makefile.js
@@ -19,6 +19,7 @@ import * as builtin from "@onlyoffice/documentation-declarations/builtin.js"
 import { parseDeclaration } from "@onlyoffice/documentation-declarations/jsdoc.js"
 import { sortJSON, prettifyJSON } from "@onlyoffice/documentation-scripts/jq.js"
 import esMain from "es-main"
+import { ESLint } from "eslint"
 import MultiStream from "multistream"
 import sade from "sade"
 import Chain from "stream-chain"
@@ -30,6 +31,7 @@ import pack from "./package.json" assert { type: "json" }
 
 const root = fileURLToPath(new URL(".", import.meta.url))
 const make = sade("./makefile.js")
+const eslint = createEslint()
 
 make
   .command("build")
@@ -259,9 +261,9 @@ function transformPassThrough(ch, enc, cb) {
 
 /**
  * @param {any} v
- * @returns {Declaration=}
+ * @returns {Promise<Declaration | undefined>}
  */
-function createDeclaration(v) {
+async function createDeclaration(v) {
   if (!(
     Object.hasOwn(v, "meta") &&
     Object.hasOwn(v.meta, "file") &&
@@ -280,11 +282,235 @@ function createDeclaration(v) {
   const longname = v.longname.replace(/\<anonymous\>~?/, "")
   d.id = [v.meta.file, longname].join(";")
 
+  if (d.description !== undefined) {
+    d.description.syntax = "md"
+  }
+
+  if (d.properties !== undefined) {
+    d.properties = d.properties.map((p) => {
+      if (p.description !== undefined) {
+        p.description.syntax = "md"
+      }
+      return p
+    })
+  }
+
+  if (d.parameters !== undefined) {
+    d.parameters = d.parameters.map((p) => {
+      if (p.description !== undefined) {
+        p.description.syntax = "md"
+      }
+      return p
+    })
+  }
+
+  if (d.returns !== undefined) {
+    d.returns = d.returns.map((p) => {
+      if (p.description !== undefined) {
+        p.description.syntax = "md"
+      }
+      return p
+    })
+  }
+
   if (d.memberof !== undefined) {
     d.memberof = d.memberof.replace(/\<anonymous\>~?/, "")
   }
 
+  if (d.examples !== undefined) {
+    d.examples = await Promise.all(d.examples.map(async (e) => {
+      const [r] = await eslint.lintText(e.text)
+      if (r.output !== undefined) {
+        e.text = r.output
+      }
+      return e
+    }))
+  }
+
   return d
+}
+
+/**
+ * @returns {ESLint}
+ */
+function createEslint() {
+  return new ESLint({
+    useEslintrc: false,
+    fix: true,
+    overrideConfig: {
+      extends: [
+        "eslint:recommended"
+      ],
+      parserOptions: {
+        sourceType: "module",
+        ecmaVersion: "latest",
+      },
+      plugins: [
+        "@stylistic/js"
+      ],
+      rules: {
+        "no-undef": "off",
+        "no-var": "warn",
+        "prefer-const": "warn",
+        "prefer-arrow-callback": "warn",
+        "@stylistic/js/array-bracket-newline": [
+          "warn",
+          { "multiline": true, "minItems": null }
+        ],
+        "@stylistic/js/array-bracket-spacing": [
+          "warn",
+          "never"
+        ],
+        "@stylistic/js/array-element-newline": [
+          "warn",
+          "always"
+        ],
+        "@stylistic/js/arrow-spacing": "warn",
+        "@stylistic/js/block-spacing": "warn",
+        "@stylistic/js/brace-style": "warn",
+        "@stylistic/js/comma-dangle": [
+          "warn",
+          "never"
+        ],
+        "@stylistic/js/comma-spacing": [
+          "warn",
+          {
+            "before": false, "after": true
+          }
+        ],
+        "@stylistic/js/comma-style": [
+          "warn",
+          "last"
+        ],
+        "@stylistic/js/function-call-argument-newline": [
+          "warn",
+          "consistent"
+        ],
+        "@stylistic/js/function-call-spacing": [
+          "warn",
+          "never"
+        ],
+        "@stylistic/js/function-paren-newline": [
+          "warn",
+          "multiline"
+        ],
+        "@stylistic/js/implicit-arrow-linebreak": [
+          "warn",
+          "beside"
+        ],
+        "@stylistic/js/indent": [
+          "warn",
+          2,
+          {
+            "VariableDeclarator": "first",
+            "FunctionDeclaration": {
+              "parameters": "first"
+            },
+            "FunctionExpression": {
+              "parameters": "first"
+            },
+            "CallExpression": {
+              "arguments": "first"
+            },
+            "ArrayExpression": "first",
+            "ObjectExpression": "first",
+            "ImportDeclaration": "first",
+            "flatTernaryExpressions": true
+          }
+        ],
+        "@stylistic/js/key-spacing": [
+          "warn",
+          {
+            "beforeColon": false, "mode": "strict"
+          }
+        ],
+        "@stylistic/js/keyword-spacing": [
+          "warn",
+          {
+            "before": true
+          }
+        ],
+        "@stylistic/js/lines-between-class-members": [
+          "warn",
+          "always"
+        ],
+        "@stylistic/js/max-len": [
+          "warn",
+          {
+            "code": 120
+          }
+        ],
+        "@stylistic/js/multiline-ternary": [
+          "warn",
+          "never"
+        ],
+        "@stylistic/js/new-parens": "warn",
+        "@stylistic/js/no-extra-semi": "warn",
+        "@stylistic/js/no-mixed-spaces-and-tabs": "warn",
+        "@stylistic/js/no-multi-spaces": "warn",
+        "@stylistic/js/no-multiple-empty-lines": "warn",
+        "@stylistic/js/no-tabs": "warn",
+        "@stylistic/js/no-trailing-spaces": "warn",
+        "@stylistic/js/no-whitespace-before-property": "warn",
+        "@stylistic/js/nonblock-statement-body-position": [
+          "warn",
+          "beside"
+        ],
+        "@stylistic/js/object-curly-newline": [
+          "warn",
+          {
+            "consistent": true
+          }
+        ],
+        "@stylistic/js/object-curly-spacing": [
+          "warn",
+          "always"
+        ],
+        "@stylistic/js/object-property-newline": [
+          "warn",
+          {
+            "allowAllPropertiesOnSameLine": true
+          }
+        ],
+        "@stylistic/js/padded-blocks": [
+          "warn",
+          "never"
+        ],
+        "@stylistic/js/quotes": [
+          "warn",
+          "double"
+        ],
+        "@stylistic/js/semi": [
+          "warn",
+          "never"
+        ],
+        "@stylistic/js/semi-spacing": "warn",
+        "@stylistic/js/space-before-blocks": "warn",
+        "@stylistic/js/space-before-function-paren": [
+          "warn",
+          { "anonymous": "always", "named": "never", "asyncArrow": "always" }
+        ],
+        "@stylistic/js/space-in-parens": [
+          "warn",
+          "never"
+        ],
+        "no-unused-vars": [
+          "warn",
+          {
+            "vars": "local"
+          }
+        ],
+        "@stylistic/js/eol-last": [
+          "warn",
+          "never"
+        ]
+      },
+      env: {
+        browser: true,
+        es2022: true
+      }
+    }
+  })
 }
 
 if (esMain(import.meta)) {
