89c0b32cd701f0b14333eeff8d016c6aaf1efd14
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-03-18 12:39:28 +0400

    preview

--- makefile.js
+++ makefile.js
@@ -2,6 +2,8 @@
 // @ts-check
 
 import { argv } from "node:process"
+import { Cli, Logger } from "@11ty/eleventy-dev-server/cli.js"
+import EleventyDevServer from "@11ty/eleventy-dev-server/server.js"
 import sade from "sade"
 import * as resources from "./resources/makefile.js"
 import * as iconJS from "./ui/icon/makefile.js"
@@ -14,6 +16,8 @@ function main() {
   sade("./makefile.js")
     .command("build")
     .action(build)
+    .command("preview-storybook")
+    .action(previewStorybook)
     .parse(argv)
 }
 
@@ -28,6 +32,23 @@ async function build() {
     iconJS.build(),
     logoJS.build()
   ])
+}
+
+/**
+ * @returns {Promise<void>}
+ */
+async function previewStorybook() {
+  const o = {
+    ...Cli.getDefaultOptions(),
+    domDiff: false,
+    input: "dist",
+    liveReload: false,
+    logger: Logger,
+    showVersion: true
+  }
+  EleventyDevServer
+    .getServer("eleventy-dev-server-cli", o.input, o)
+    .serve(o.port)
 }
 
 main()
