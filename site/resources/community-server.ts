e054718816e8968b69d0ad80615d3286111fa941
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-03-20 18:04:37 +0400

    uncomment resources

--- site/resources/community-server.ts
+++ site/resources/community-server.ts
@@ -3,12 +3,14 @@ import { isBuild, isPreview } from "../config/mode.ts"
 
 const require = createRequire(import.meta.url)
 
-const r = false // isBuild() || isPreview()
+const r = isBuild() || isPreview()
   ? require("@onlyoffice/documentation-resources/community-server.cjs")
   : require("@onlyoffice/documentation-declarations-fixtures/rest.js")
 
 export function list() {
-  return r.list()
+  // todo: implement it.
+  return []
+  // return r.list()
 }
 
 export function retrieve(id) {
--- site/resources/document-builder.ts
+++ site/resources/document-builder.ts
@@ -3,7 +3,7 @@ import { isBuild, isPreview } from "../config/mode.ts"
 
 const require = createRequire(import.meta.url)
 
-const r = true // isBuild() || isPreview()
+const r = isBuild() || isPreview()
   ? require("@onlyoffice/documentation-resources/document-builder.cjs")
   : require("@onlyoffice/documentation-declarations-fixtures/code.js")
 
