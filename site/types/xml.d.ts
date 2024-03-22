7f25d53137944f4543861d124bd8e24fd20f14f7
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-03-22 12:22:14 +0400

    add xml types

---
+++ site/types/xml.d.ts
@@ -0,0 +1,14 @@
+declare global {
+  namespace preact {
+    namespace JSX {
+      interface IntrinsicElements {
+        urlset: any
+        url: any
+        loc: any
+        lastmod: any
+      }
+    }
+  }
+}
+
+export {}
