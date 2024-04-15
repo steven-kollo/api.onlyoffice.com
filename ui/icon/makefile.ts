c50d8e15bbc17a50201c0b5564a2d6ef0eb046a6
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-04-15 10:37:24 +0400

    up ui scripts

--- ui/icon/makefile.ts
+++ ui/icon/makefile.ts
@@ -1,37 +1,28 @@
-#!/usr/bin/env node
-// @ts-check
-
 import { mkdir } from "node:fs/promises"
 import { existsSync } from "node:fs"
 import { join } from "node:path"
 import { argv } from "node:process"
 import { URL, fileURLToPath } from "node:url"
-import { buildTSX } from "@onlyoffice/documentation-utils/svg.js"
+import { buildTSX } from "@onlyoffice/documentation-utils/svg.ts"
 import sade from "sade"
 import esMain from "es-main"
 
 const root = fileURLToPath(new URL(".", import.meta.url))
 const lib = join(root, "lib")
 
-/**
- * @returns {void}
- */
-function main() {
+function main(): void {
   sade("./makefile.js")
     .command("build")
     .action(build)
     .parse(argv)
 }
 
-/**
- * @returns {Promise<void>}
- */
-export async function build() {
+export async function build(): Promise<void> {
   if (!existsSync(lib)) {
     await mkdir(lib)
   }
   const m = join(root, "static")
-  await buildTSX(m, lib, "logo")
+  await buildTSX(m, lib, "icon")
 }
 
 if (esMain(import.meta)) {
