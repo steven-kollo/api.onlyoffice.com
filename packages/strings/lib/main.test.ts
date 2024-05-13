b30cfe5b15beb62149af3abbfe9c6b394bfa8525
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-05-13 16:52:14 +0400

    add the selectSection util

--- packages/strings/lib/main.test.ts
+++ packages/strings/lib/main.test.ts
@@ -1,6 +1,6 @@
 import {is} from "uvu/assert"
 import {test} from "uvu"
-import {firstParagraph, firstSentence, isStringLiteral} from "./main.js"
+import {firstParagraph, firstSentence, isStringLiteral, selectSection} from "./main.js"
 
 test("firstParagraph() returns an empty string for an empty content", () => {
   const e = firstParagraph("")
@@ -20,6 +20,36 @@ test("firstParagraph() returns a first paragraph for a content with multiple par
 test("firstParagraph() returns a first paragraph for a content with a heading", () => {
   const e = firstParagraph("# h\n\na\n\nb")
   is(e, "a")
+})
+
+test("selectSection() returns an empty string for an empty title", () => {
+  const e = selectSection("", "c")
+  is(e, "")
+})
+
+test("selectSection() returns an empty string for an empty content", () => {
+  const e = selectSection("t", "")
+  is(e, "")
+})
+
+test("selectSection() returns an empty string for a title not found in the content", () => {
+  const e = selectSection("t", "c")
+  is(e, "")
+})
+
+test("selectSection() returns a section for a title found in the content", () => {
+  const e = selectSection("t", "## t\n\nc")
+  is(e, "c")
+})
+
+test("selectSection() returns a section for a title found in the content with multiple sections", () => {
+  const e = selectSection("t", "## t\n\nc\n\n## g\n\nd")
+  is(e, "c")
+})
+
+test("selectSection() returns the first section for a title found multiple times in the content", () => {
+  const e = selectSection("t", "## t\n\nc\n\n## t\n\nd")
+  is(e, "c")
 })
 
 test("firstSentence() returns an empty string for an empty content", () => {
--- packages/strings/lib/main.ts
+++ packages/strings/lib/main.ts
@@ -1,9 +1,11 @@
 import type {Paragraph} from "mdast"
 import {fromMarkdown} from "mdast-util-from-markdown"
 import {toMarkdown} from "mdast-util-to-markdown"
+import {toString} from "mdast-util-to-string"
 import {English} from "sentence-splitter/lang"
 import {split} from "sentence-splitter"
 import {select} from "unist-util-select"
+import {remove} from "unist-util-remove"
 
 export function firstParagraph(s: string): string {
   const t = fromMarkdown(s)
@@ -15,6 +17,36 @@ export function firstParagraph(s: string): string {
     }
   }
   return s
+}
+
+export function selectSection(t: string, c: string): string {
+  const r = fromMarkdown(c)
+
+  let i = -1
+  remove(r, (node, index) => {
+    if (node.type === "root") {
+      return false
+    }
+    if (node.type === "heading" && "depth" in node && node.depth === 2) {
+      const s = toString(node)
+      if (s === t && index !== undefined && i === -1) {
+        i = index
+        return true
+      }
+      i = -1
+    }
+    if (i === -1) {
+      return true
+    }
+    return false
+  })
+
+  c = toMarkdown(r)
+  if (c.endsWith("\n")) {
+    c = c.slice(0, -1)
+  }
+
+  return c
 }
 
 export function firstSentence(s: string): string {
