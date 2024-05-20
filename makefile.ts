550bf62cefc189eda7cf4b9102b2acc45f1f00fb
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-05-20 11:15:56 +0400

    add root test command

--- makefile.ts
+++ makefile.ts
@@ -1,4 +1,8 @@
+import {spawn} from "node:child_process"
+import {readFile, readdir} from "node:fs/promises"
+import {join} from "node:path"
 import {argv} from "node:process"
+import {URL, fileURLToPath} from "node:url"
 import sade from "sade"
 import * as resources from "./resources/makefile.ts"
 import * as iconJS from "./ui/icon/makefile.ts"
@@ -10,6 +14,8 @@ function main(): void {
   sade("./makefile.js")
     .command("build")
     .action(build)
+    .command("test")
+    .action(test)
     .parse(argv)
 }
 
@@ -21,4 +27,40 @@ async function build(): Promise<void> {
     iconJS.build(),
     logoJS.build()
   ])
+}
+
+async function test(): Promise<void> {
+  const cd = currentDir()
+  const pd = packagesDir(cd)
+  const pc = await readdir(pd)
+  for (const p of pc) {
+    const d = join(pd, p)
+    const f = packageJSON(d)
+    const c = await readFile(f, "utf8")
+    const j = JSON.parse(c)
+    if (!j.scripts) {
+      continue
+    }
+    if (!j.scripts.test) {
+      continue
+    }
+    await new Promise((res, rej) => {
+      const s = spawn("pnpm", ["test"], {cwd: d, stdio: "inherit"})
+      s.on("close", res)
+      s.on("error", rej)
+    })
+  }
+}
+
+function currentDir(): string {
+  const u = new URL(".", import.meta.url)
+  return fileURLToPath(u)
+}
+
+function packagesDir(d: string): string {
+  return join(d, "packages")
+}
+
+function packageJSON(d: string): string {
+  return join(d, "package.json")
 }
