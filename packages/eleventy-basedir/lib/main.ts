570849fdbb0664e2c440da7a9562446b202fecbe
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-05-20 09:33:23 +0400

    remove eleventy-basedir

--- packages/eleventy-basedir/lib/main.ts
+++
@@ -1,9 +0,0 @@
-import {env} from "node:process"
-
-export function rootDir(): string {
-  const r = env.ELEVENTY_ROOT
-  if (r === undefined) {
-    throw new Error("ELEVENTY_ROOT is not set")
-  }
-  return r
-}
