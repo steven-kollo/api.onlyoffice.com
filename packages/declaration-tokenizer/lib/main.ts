54b95774b666ab61128862d07d64e39e8b12fbda
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-05-20 09:22:49 +0400

    reorder functions

--- packages/declaration-tokenizer/lib/main.ts
+++ packages/declaration-tokenizer/lib/main.ts
@@ -12,12 +12,24 @@ export interface DecoratorToken extends TokenNode {
   type: "decorator"
 }
 
+export function decoratorToken(t: TokenNode): DecoratorToken {
+  return {...t, type: "decorator"}
+}
+
 export interface IdentifierToken extends TokenNode {
   type: "identifier"
 }
 
+export function identifierToken(t: TokenNode): IdentifierToken {
+  return {...t, type: "identifier"}
+}
+
 export interface KeywordToken extends TokenNode {
   type: "keyword"
+}
+
+export function keywordToken(t: TokenNode): KeywordToken {
+  return {...t, type: "keyword"}
 }
 
 export interface ReferenceToken extends TokenNode {
@@ -25,33 +37,21 @@ export interface ReferenceToken extends TokenNode {
   id: string
 }
 
+export function referenceToken(t: TokenNode): ReferenceToken {
+  return {...t, type: "reference", id: ""}
+}
+
 export interface TextToken extends TokenNode {
   type: "text"
+}
+
+export function textToken(t: TokenNode): TextToken {
+  return {...t, type: "text"}
 }
 
 export interface TokenNode {
   type: string
   text: string
-}
-
-export function decoratorToken(t: TokenNode): DecoratorToken {
-  return {...t, type: "decorator"}
-}
-
-export function identifierToken(t: TokenNode): IdentifierToken {
-  return {...t, type: "identifier"}
-}
-
-export function keywordToken(t: TokenNode): KeywordToken {
-  return {...t, type: "keyword"}
-}
-
-export function referenceToken(t: TokenNode): ReferenceToken {
-  return {...t, type: "reference", id: ""}
-}
-
-export function textToken(t: TokenNode): TextToken {
-  return {...t, type: "text"}
 }
 
 export function tokenNode(): TokenNode {
